5e71aeeaf27242a966f3efcb806d491eda15822f
Lucas Santana <lcs30-09@hotmail.com>
2019-11-11 14:55:40 -0300

    construindo primeira tela e entendendo o kivy

---
+++ Computador/main.py
@@ -0,0 +1,25 @@
+from FunctionsGeneral import LanguagePacket
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Constructor_Borders = 1
+class InitScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+
+class DDHMapp(App):
+    Text = LanguagePacket()
+    LanguageTex = Text.DictLanguage()
+    def build(self):
+        self.load_kv('DDHM.kv')
+        sm = ScreenManager()
+        sm.add_widget(InitScreen(name='menu'))
+        sm.add_widget(SettingsScreen(name='settings'))
+        return sm
+
+
+if __name__ == '__main__':
+    DDHMapp().run() 
