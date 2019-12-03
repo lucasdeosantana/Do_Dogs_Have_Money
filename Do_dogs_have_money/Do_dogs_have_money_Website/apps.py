26ccbfaee18f6861b4bb8e52640430e06a0b6b3f
Lucas <lcs30-09@hotmail.com>
2019-12-02 23:00:36 -0200

    intermediario para novo site

--- Do_dogs_have_money/Do_dogs_have_money_Website/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DoDogsHaveMoneyWebsiteConfig(AppConfig):
-    name = 'Do_dogs_have_money_Website'
--- Do_dogs_have_money/Do_dogs_have_money_Website/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-	return render(request, 'Do_dogs_have_money_Website/index.html')
